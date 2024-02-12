3539b6884ed7864b4f42bc109a801c2d608fe281
Nickolay1986 <kolya-grek@list.ru>
2024-02-13 00:21:23 +0600

    feat: add function getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -404,8 +404,11 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  if (!arr.length) {
+    return 0;
+  }
+  return arr.reduce((count, value) => count + (value ? 0 : 1), 0);
 }
 
 /**
