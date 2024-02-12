3a02af2c2a08c963e9102e27ca89d96ce8115535
Nickolay1986 <kolya-grek@list.ru>
2024-02-12 13:26:32 +0600

    feat: add function isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -145,8 +145,12 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) {
+    return true;
+  }
+  const firstStringLength = arr[0].length;
+  return arr.every((str) => str.length === firstStringLength);
 }
 
 /**
